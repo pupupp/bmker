d295fd26c4af0c8717fb0ff4c35f8f37dad94bd2
pupupp <87363040+pupupp@users.noreply.github.com>
2022-10-22 09:25:36 +0800

    Add files via upload

---
+++ 实验二 学生选课模拟系统/java第二次实验/Xuanke/Teacher.java
@@ -0,0 +1,14 @@
+package xuanke;
+
+public class Teacher {
+	String number;
+    String name;
+    String sex;
+    Course[] course;
+    public Teacher(String a,String b,String c,Course[] d){
+        number=a;
+        name=b;
+        sex=c;
+        course=d;
+    }
+}
