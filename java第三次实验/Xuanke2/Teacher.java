cf34b37d490f6bce7c04c7af549aea3a76c012b9
pupupp <87363040+pupupp@users.noreply.github.com>
2022-10-22 09:13:58 +0800

    Add files via upload

---
+++ java第三次实验/Xuanke2/Teacher.java
@@ -0,0 +1,10 @@
+package xuanke;
+
+public class Teacher extends People {
+    public Teacher(String a,String b,String c,Course[] d){
+        number=a;
+        name=b;
+        sex=c;
+        course=d;
+    }
+}
